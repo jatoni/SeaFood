d1dde8608f7126006b0c4db125ad3eff09d06771
GungryNutria <edgardochamorro08@gmail.com>
2023-11-28 21:20:13 -0600

    feat :sparkles: Master | Implement Java swing and Register View

--- src/main/java/com/mx/sea/food/SeaFood/App.java
+++ src/main/java/com/mx/sea/food/SeaFood/App.java
@@ -1,12 +1,13 @@
 package com.mx.sea.food.SeaFood;
 
+import com.mx.sea.food.views.Register;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		
-
+		new Register();
 	}
 }
