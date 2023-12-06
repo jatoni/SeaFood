119c92e12de7d59c381b17eb2a22108d0788b73a
jatoni <jat_antonio_torres.juan@outlook.com>
2023-12-05 20:28:23 -0600

    Feat :sparkles: master | se agrega menu al dashboard

--- src/main/java/com/mx/sea/food/views/Dashboard.java
+++ src/main/java/com/mx/sea/food/views/Dashboard.java
@@ -3,8 +3,17 @@ package com.mx.sea.food.views;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 
 import com.mx.sea.food.dto.EmployeeDto;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Dashboard {
 
@@ -40,8 +49,34 @@ public class Dashboard {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 770, 510);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
 
+		JMenuItem i1, i2, i3, i4, i5;
+		JMenu usuarios, productos;
+		JMenuBar mb = new JMenuBar();
+
+		usuarios = new JMenu("Usuarios");
+		productos = new JMenu("Productos");
+		productos.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				productoDashboard.setVisible(true);
+			}
+		});
+
+		mb.add(usuarios);
+		mb.add(productos);
+		frame.setJMenuBar(mb);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 754, Short.MAX_VALUE)
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGap(0, 449, Short.MAX_VALUE)
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
 }
