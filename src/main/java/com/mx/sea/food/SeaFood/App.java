e3903b06ce90b37c1b03a2cd8999e8b1fe244f8c
jatoni <jat_antonio_torres.juan@outlook.com>
2023-12-09 19:08:33 -0600

    Se perdio el dashboard

--- src/main/java/com/mx/sea/food/SeaFood/App.java
+++ src/main/java/com/mx/sea/food/SeaFood/App.java
@@ -1,17 +1,15 @@
 package com.mx.sea.food.SeaFood;
 
 import com.mx.sea.food.views.Login;
+import com.mx.sea.food.views.Register;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		new Register();
-		
-=======
 		new Login().run();
->>>>>>> 48b0998aa5904e71b937d84f6227b8adfde5bde7
 	}
 }
