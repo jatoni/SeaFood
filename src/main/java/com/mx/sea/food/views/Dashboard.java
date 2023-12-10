8d7ff822d91c8e335d8f0a2661f47050871ccb34
jatoni <jat_antonio_torres.juan@outlook.com>
2023-12-10 12:46:56 -0600

    Se hacen modificaciones en el tabla de productos

--- src/main/java/com/mx/sea/food/views/Dashboard.java
+++ src/main/java/com/mx/sea/food/views/Dashboard.java
@@ -7,18 +7,24 @@ import javax.swing.JMenu;
 
 import com.mx.sea.food.dto.EmployeeDto;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.JPanel;
-import java.awt.Color;
+import javax.swing.JTable;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.table.DefaultTableModel;
 
 public class Dashboard {
 
 	private JFrame frame;
 	private EmployeeDto _employee;
+	Object[][] data = {};
+	String[] titulos = { "id", "Producto", "Fecha de Ingreso", "Tipos de producto", "Stock" };
+	private JTable productostable;
+	private DefaultTableModel productosmodeloTable;
 
 	/**
 	 * Launch the application.
@@ -48,11 +54,13 @@ public class Dashboard {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+
+		productostable = new JTable();
+
 		frame = new JFrame();
 		frame.setBounds(100, 100, 770, 510);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		JMenuItem i1, i2, i3, i4, i5;
 		JMenu usuarios, productos;
 		JMenuBar mb = new JMenuBar();
 
@@ -68,11 +76,35 @@ public class Dashboard {
 		mb.add(usuarios);
 		mb.add(productos);
 		frame.setJMenuBar(mb);
+
+		productosmodeloTable = new DefaultTableModel(titulos, 0) {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+
+		productostable = new JTable(productosmodeloTable);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(UIManager.getColor("Button.shadow"));
 		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
-		groupLayout
-				.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGap(0, 754, Short.MAX_VALUE));
-		groupLayout
-				.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addGap(0, 449, Short.MAX_VALUE));
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(panel,
+				Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 754, Short.MAX_VALUE));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING).addComponent(panel,
+				Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 449, Short.MAX_VALUE));
+
+		GroupLayout gl_panel = new GroupLayout(panel);
+		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGap(48)
+						.addComponent(productostable, GroupLayout.PREFERRED_SIZE, 656, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(50, Short.MAX_VALUE)));
+		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel.createSequentialGroup().addGap(64)
+						.addComponent(productostable, GroupLayout.PREFERRED_SIZE, 271, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(114, Short.MAX_VALUE)));
+		panel.setLayout(gl_panel);
 		frame.getContentPane().setLayout(groupLayout);
+
 	}
 }
